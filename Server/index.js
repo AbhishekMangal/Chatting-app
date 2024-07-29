b27cbaf3096db5e17763b234886c21d385f3bd17
AbhishekMangal <149200926+AbhishekMangal@users.noreply.github.com>
2024-07-29 18:17:36 +0530

    third commit

--- Server/index.js
+++ Server/index.js
@@ -1,68 +1,57 @@
 const express = require('express');
-const connectToMongo = require('./db')
-var cors = require('cors');
+const connectToMongo = require('./db');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require("socket.io");
+
 const app = express();
-const socket = require("socket.io");
-
 app.use(express.json());
-app.use(cors(
-{
+app.use(cors({
   origin: "https://chatting-app-11.onrender.com",
+  methods: ["GET", "POST"],
   credentials: true,
-}
-));
-const httpServer = require('http').createServer(app);
+}));
 
+const httpServer = http.createServer(app);
 
-const port = 5000;
 require('dotenv').config();
-app.use('/api/auth', require('./Routes/userRoutes'))
-app.use('/api/message', require('./Routes/messagesRoute'))
-app.use('/api/friend', require('./Routes/FriendsRoute'))
 
-app.use('/api/request', require('./Routes/Request'))
-// app.get('/',async(req, res)=>
-// {
-//     await res.send("Home page")
-// })
-const server = app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
-connectToMongo();
-const io =  socket(server, {
-  cors:{
-    origin:"https://chatting-app-11.onrender.com/",
-  
-    Credential: true,
+app.use('/api/auth', require('./Routes/userRoutes'));
+app.use('/api/message', require('./Routes/messagesRoute'));
+app.use('/api/friend', require('./Routes/FriendsRoute'));
+app.use('/api/request', require('./Routes/Request'));
 
-  },
-  
+const port = process.env.PORT || 5000;
+const server = httpServer.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
 
+connectToMongo();
+
+const io = new Server(server, {
+  cors: {
+    origin: "https://chatting-app-11.onrender.com",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 global.onlineUsers = new Map();
-console.log(global.onlineUsers , "s");
-let x  = 1;
 
-io.on("connection",(socket)=>{
-
+io.on("connection", (socket) => {
+  console.log('a user connected');
   global.chatSocket = socket;
-  socket.on("add-user", (userId)=>{
-    console.log(userId +"s");
+  socket.on("add-user", (userId) => {
     onlineUsers.set(userId, socket.id);
-  })
-  socket.on("send-msg", (data)=>{
-    
+  });
+  socket.on("send-msg", (data) => {
     const sendUserSocket = onlineUsers.get(data.to);
-    
-    if(sendUserSocket){
-      
-      
-      socket.to(sendUserSocket).emit("msg-recieve",data.message,  data.length);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.message, data.length);
       socket.to(sendUserSocket).emit("new-message", {
         from: data.from,
         message: data.message,
       });
     }
-  })
-} )
+  });
+});
