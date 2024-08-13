ad0b9b6826745803d0d6b630f33ee47d5984e821
AbhishekMangal <149200926+AbhishekMangal@users.noreply.github.com>
2024-08-14 00:22:01 +0530

    final commit

--- Server/db.js
+++ Server/db.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const connectToMongo = async(username, password)=> {
     try {
        
-        // await mongoose.connect(`mongodb+srv://${username}:Abhishek@cluster0.uhxsifh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
-        await mongoose.connect("mongodb://localhost:27017/Chat-app")
+        await mongoose.connect(`mongodb+srv://${username}:Abhishek@cluster0.uhxsifh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+        // await mongoose.connect("mongodb://localhost:27017/Chat-app")
         console.log("Connected Succefully");
     } catch (error) {
         console.error(error.message)
